fb318a6e0e22e0c057ee0cdaadf55e22c600d559
Anish Patel <anish.patel2@rms.com>
2015-08-26 11:44:49 +0100

    Adding a basic function and setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='rms-python-testpackage',
+      version='0.1',
+      description='The answer to the ultimate question',
+      url='https://github.com/apatel-rms/rms-python-testpackage.git',
+      author='Anish Patel',
+      author_email='anish.patel2@rms.com',
+      license='MIT',
+      packages=['rms-python-testpackage'],
+      zip_safe=False)
